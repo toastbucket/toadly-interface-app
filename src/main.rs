a6e39522df7cb9d0011499e8213198bab1ca2531
Nick Elsmore <nicholaselsmore@gmail.com>
2024-05-28 14:46:56 -0700

    improve ui layout

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ slint::include_modules!();
 fn main() {
     let mw = MainWindow::new().unwrap();
 
-    mw.set_house_batt_level(0.3);
+    mw.set_house_batt_level(0.4567);
     mw.set_fresh_tank_level(0.9);
     mw.set_grey_tank_level(0.2);
 
