a2ffc13d1c994355e287b5fefbaaf912c8543aa4
Nick Elsmore <nicholaselsmore@gmail.com>
2024-05-30 00:44:44 -0700

    mock up elements

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ slint::include_modules!();
 fn main() {
     let mw = MainWindow::new().unwrap();
 
-    mw.set_house_batt_level(0.4567);
+    mw.set_house_batt_level(0.142);
+    mw.set_truck_batt_voltage(12.5642);
     mw.set_fresh_tank_level(0.9);
     mw.set_grey_tank_level(0.2);
 
