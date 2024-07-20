0d36b56324748f22cce9bdb0a68bc05d2438658e
Nick Elsmore <nicholaselsmore@gmail.com>
2024-07-19 22:02:33 -0700

    build vedirect parser

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,96 @@
+mod vedirect;
+
+use std::io::ErrorKind;
+use std::sync::mpsc;
+use std::time::Duration;
+use vedirect::{Register, VeDirectDevice, VeDirectParser};
+
 slint::include_modules!();
+
+#[derive(Debug)]
+enum SensorMessage {
+    VeDirectMessage { r: Register, d: VeDirectDevice },
+}
+
+fn dispatch_vedirect_message(mw: &MainWindow, r: Register, d: VeDirectDevice) {
+    match d {
+        VeDirectDevice::BMV71xSmartShunt => {
+            match r {
+                Register::AuxVoltage(v) => {
+                    mw.set_truck_batt_voltage(v);
+                },
+                Register::StateOfCharge(soc) => {
+                    mw.set_house_batt_level(soc / 100f32);
+                },
+                _ => (),
+            }
+        },
+        VeDirectDevice::MPPT => {
+            match r {
+                Register::PanelPower(p) => {
+                    mw.set_solar_value(p);
+                }
+                _ => (),
+            }
+        },
+        VeDirectDevice::PhoenixInverter => {
+            match r {
+                Register::ACOutputApparentPower(p) => {
+                    mw.set_inverter_value(p);
+                },
+                _ => (),
+            }
+        },
+        _ => println!("invalid VeDirectDevice: {:?}", d),
+    }
+}
 
 fn main() {
     let mw = MainWindow::new().unwrap();
     let mw_w = mw.as_weak();
+    let (sender, receiver) = mpsc::channel::<SensorMessage>();
 
     let _ = std::thread::spawn(move || {
-        let mut house_batt = 1.0;
-        let mut truck_batt = 0.0;
-        let mut fresh_tank = 1.0;
-        let mut grey_tank = 0.0;
-        let mut inverter_value = 0.0;
-        let mut solar_value = 0.0;
+        loop {
+            if let Ok(msg) = receiver.recv() {
+                let _ = mw_w.upgrade_in_event_loop(move |mw| {
+                    match msg {
+                        SensorMessage::VeDirectMessage { r, d } => {
+                            dispatch_vedirect_message(&mw, r, d);
+                        },
+                        _ => (),
+                    }
+                });
+            }
+        }
+    });
+
+    let _ = std::thread::spawn(move || {
+        let mut port = serialport::new("/tmp/vmodem0", 19_200)
+            .timeout(Duration::from_millis(250))
+            .open().expect("failed to open port");
+        let mut veparser = VeDirectParser::new();
 
         loop {
-            let _ = mw_w.upgrade_in_event_loop(move |mw| {
-                mw.set_house_batt_level(house_batt);
-                mw.set_truck_batt_voltage(truck_batt);
-                mw.set_fresh_tank_level(fresh_tank);
-                mw.set_grey_tank_level(grey_tank);
-                mw.set_inverter_value(inverter_value);
-                mw.set_solar_value(inverter_value);
-            });
-
-            if house_batt > 0.0 {
-                house_batt -= 0.05;
+            let mut buf = [0; 128];
+            match port.read(&mut buf) {
+                Ok(n) => {
+                    let to_push: Option<&[u8]> = if n > 0 {Some(&buf)} else {None};
+                    if let (Some(regs), Some(dev)) = (veparser.push(to_push), veparser.device()) {
+                        for r in regs {
+                            let _ = sender.send(SensorMessage::VeDirectMessage { r: r, d: dev });
+                        }
+                    }
+                },
+                Err(e) => {
+                    if e.kind() == ErrorKind::TimedOut {
+                        continue;
+                    } else {
+                        println!("{}", e);
+                        break;
+                    }
+                },
             }
-
-            if truck_batt < 14.0 {
-                truck_batt += 0.5;
-            }
-
-            if fresh_tank > 0.0 {
-                fresh_tank -= 0.05;
-            }
-
-            if grey_tank < 1.0 {
-                grey_tank += 0.05;
-            }
-
-            if inverter_value < 1000.0 {
-                inverter_value += 10.0;
-            }
-
-            if solar_value < 300.0 {
-                solar_value += 10.0;
-            }
-
-            std::thread::sleep(std::time::Duration::from_millis(1000));
         }
     });
 
